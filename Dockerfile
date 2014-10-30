d937df0bf2c4465088fec1218038cd6641dafa14
Michał Rostecki <nihilifer@nihilifer.net>
2014-10-30 19:36:40 -0400

    postgres

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM centos
 
 RUN rpm -Uhv http://ftp.pbone.net/pub/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
-RUN yum install -y python-pip python-devel gcc
+RUN yum install -y python-pip python-devel postgresql-devel gcc
 
 ADD . /opt/shmir-website
 
@@ -9,5 +9,5 @@ RUN pip install -r /opt/shmir-website/requirements.txt
 
 WORKDIR /opt/shmir-website/shweb
 
-RUN python manage.py syncdb --noinput
+#RUN python manage.py syncdb --noinput
 RUN python manage.py collectstatic --noinput
--- shweb/shweb/settings.py
+++ shweb/shweb/settings.py
@@ -59,10 +59,21 @@ WSGI_APPLICATION = 'shweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        # 'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
